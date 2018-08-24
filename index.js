1b81def69ac3ddf9ecb2c9fb16077eef7722867f
Gina Fedder <ginafedder@yahoo.com>
2018-08-24 19:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase()) {
-    return "I can\'t hear you!"
+  if (string.toUpperCase()) {
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
   else {
-    return "YES, INDEED!"
+    return "I can\'t hear you!"
   }
 }
