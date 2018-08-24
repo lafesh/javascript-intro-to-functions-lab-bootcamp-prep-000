4e8dc5f73f4c2030845559190e05a3348273733f
Gina Fedder <ginafedder@yahoo.com>
2018-08-24 19:48:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma.") {
     return "I love you,too."
   } else if (string.toUpperCase()) {
-    return "YES INDEED!"
+    return "I can\'t hear you!"
   }
   else {
-    return "I can\'t hear you!"
+    return "YES INDEED!"
   }
 }
